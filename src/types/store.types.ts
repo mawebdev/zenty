cf570a7b3928054720f106e86136b1a25954b495
Milaim Ajdari <milaim.ajdari@gmail.com>
2025-07-01 19:13:48 +0200

    added many functions

--- src/types/store.types.ts
+++ src/types/store.types.ts
@@ -4,9 +4,12 @@ export type EntityStoreOptions<T> = {
 };
 
 export type EntitiesStoreOptions<T extends { id: string | number }> = {
-  initialState: T[];
+  initialState?: T[];
   create?: (item: T, state: { entities: T[] }) => T[];
+  createMany?: (items: T[], state: { entities: T[] }) => T[];
   update?: (uid: string | number, item: Partial<T>, state: { entities: T[] }) => T[];
+  updateMany?: (items: Partial<T>[], state: { entities: T[] }) => T[];
   delete?: (uid: string | number, state: { entities: T[] }) => T[];
+  deleteMany?: (uids: (string | number)[], state: { entities: T[] }) => T[];
   clear?: (state: { entities: T[] }) => T[];
 };
